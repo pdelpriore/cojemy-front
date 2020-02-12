b4a1ddf6e63fb28cfd86e15f0d6513bb62dfb104
pdelpriore <paul.del.priore29@gmail.com>
2020-02-12 11:25:31 +0100

    Make navbar fixed + prepare main page

--- src/views/main/Main.js
+++ src/views/main/Main.js
@@ -1,8 +1,27 @@
 import React from "react";
+import { Image, Row, Col } from "react-bootstrap";
 import "./main.css";
 
 const Main = () => {
-  return <div className="main-section"></div>;
+  return (
+    <div className="main-section">
+      <Row>
+        <Col xs>
+          <div className="shape-top"></div>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={5} />
+        <Col xs={6}>
+          <Image
+            className="image"
+            src={require("../../assets/womancooking.jpg")}
+          />
+        </Col>
+        <Col xs={1} />
+      </Row>
+    </div>
+  );
 };
 
 export default Main;
