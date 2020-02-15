39875077925ab671a96397e6d544601198146a2a
pdelpriore <paul.del.priore29@gmail.com>
2020-02-15 21:47:30 +0100

    change texts

--- src/strings/Strings.js
+++ src/strings/Strings.js
@@ -7,14 +7,14 @@ export const strings = Object.freeze({
     }
   },
   main: {
-    MAIN_TEXT: "recherchez une idée de plat ?",
-    MAIN_SUB_TEXT: "vous êtes en bon endroit",
-    BUTTON_TEXT: "commencez"
+    MAIN_TEXT: "recherche une idée de plat ?",
+    MAIN_SUB_TEXT: "tu es en bon endroit",
+    BUTTON_TEXT: "commence"
   },
   about: {
-    FIRST_AREA: "trouvez votre recette",
-    SECOND_AREA: "partagez vos recettes",
-    THIRD_AREA: "contactez d'autres passionnés",
-    FOURTH_AREA: "organisez vos événements"
+    FIRST_AREA: "trouve ta recette",
+    SECOND_AREA: "partage tes recettes",
+    THIRD_AREA: "contacte d'autres passionnés",
+    FOURTH_AREA: "organise tes événements"
   }
 });
