b1cb94505e625c813c93fffe6384c47b5523f045
pdelpriore <paul.del.priore29@gmail.com>
2020-02-18 17:28:51 +0100

    prepare login page

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,15 +1,28 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
+import { Row, Col, Button, Image } from "react-bootstrap";
+import "./login.css";
 
 const Login = ({ match }) => {
   let { path, url, isExact } = match;
   return (
-    <div>
-      <Navbar path={path} url={url} isExact={isExact} />
-      Welcome to login Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-      ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-      ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-      ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+    <div className="login-area">
+      <div className="login-first-section">
+        <Navbar path={path} url={url} isExact={isExact} />
+        <Row>
+          <Col xs={2} />
+          <Col xs={8}>
+            <Image
+              className="login-image"
+              src={require("../../assets/imgs/loginret.jpg")}
+            />
+          </Col>
+          <Col xs={2} />
+        </Row>
+      </div>
+      <div className="login-second-section">
+        <h1 className="text-test">Lorem ipsum</h1>
+      </div>
     </div>
   );
 };
