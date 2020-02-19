b4c4c7c0e3881e470d90b3b4d29a503b7a282dc9
pdelpriore <paul.del.priore29@gmail.com>
2020-02-19 14:28:07 +0100

    prepare signup page

--- src/views/signup/Signup.js
+++ src/views/signup/Signup.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
 import Navbar from "../../components/navbar/Navbar";
+import "./signup.css";
 
 const Signup = ({ match: { path, url, isExact } }) => {
-  return <></>;
+  const props = useSpring({
+    opacity: 1,
+    from: { opacity: 0 }
+  });
+  return (
+    <animated.div style={props} className="signup-area">
+      <Navbar path={path} url={url} isExact={isExact} />
+      <div className="signup-first-section"></div>
+      <div className="signup-second-section"></div>
+    </animated.div>
+  );
 };
 
 export default Signup;
