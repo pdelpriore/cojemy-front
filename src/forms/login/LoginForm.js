ebad56f467b7716fbffd7f276c03e630ff0c4e79
pdelpriore <paul.del.priore29@gmail.com>
2020-02-19 10:00:54 +0100

    connect login form hook to login form

--- src/forms/login/LoginForm.js
+++ src/forms/login/LoginForm.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Form, Row, Col, Button } from "react-bootstrap";
 import { strings } from "../../strings/Strings";
 import { capitalizeFirst } from "../../util/Util";
+import useLoginForm from "../../hooks/form/login/useLoginForm";
 import "./loginForm.css";
 
 const LoginForm = () => {
+  const { inputs, handleInputChange, handleSubmit } = useLoginForm();
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Row>
         <Col xs={12}>
           <Form.Group controlId="formBasicEmail">
@@ -14,6 +16,8 @@ const LoginForm = () => {
               {capitalizeFirst(strings.login.EMAIL)}
             </Form.Label>
             <Form.Control
+              onChange={handleInputChange}
+              value={inputs.email || ""}
               className="login-placeholder"
               size="lg"
               name="email"
@@ -30,6 +34,8 @@ const LoginForm = () => {
               {capitalizeFirst(strings.login.PASSWORD)}
             </Form.Label>
             <Form.Control
+              onChange={handleInputChange}
+              value={inputs.password || ""}
               className="login-placeholder"
               size="lg"
               name="password"
@@ -41,7 +47,17 @@ const LoginForm = () => {
       </Row>
       <Row>
         <Col xs={12}>
-          <Button className="login-button" type="submit" variant="outline-dark">
+          <Button
+            disabled={
+              inputs.email === undefined ||
+              inputs.email === "" ||
+              inputs.password === undefined ||
+              inputs.password === ""
+            }
+            className="login-button"
+            type="submit"
+            variant="outline-dark"
+          >
             {capitalizeFirst(strings.login.BUTTON_TEXT)}
           </Button>
         </Col>
