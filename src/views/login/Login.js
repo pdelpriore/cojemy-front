bdb04196514536af7fce52210b8b8308cd8b1c84
pdelpriore <paul.del.priore29@gmail.com>
2020-02-19 14:12:14 +0100

    add login form transition

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import { Row, Col, Image } from "react-bootstrap";
-import { useSpring, animated } from "react-spring";
+import { useSpring, useTransition, animated } from "react-spring";
 import LoginForm from "../../forms/login/LoginForm";
 import "./login.css";
 
@@ -9,6 +9,11 @@ const Login = ({ match: { path, url, isExact } }) => {
   const props = useSpring({
     opacity: 1,
     from: { opacity: 0 }
+  });
+  const loginForm = [<LoginForm />];
+  const transition = useTransition(loginForm, loginForm => loginForm.id, {
+    from: { opacity: 0, marginLeft: -100, marginRight: 100 },
+    enter: { opacity: 1, marginLeft: 0, marginRight: 0 }
   });
   return (
     <animated.div style={props} className="login-area">
@@ -31,7 +36,11 @@ const Login = ({ match: { path, url, isExact } }) => {
         <Row>
           <Col xs={6} />
           <Col xs={3}>
-            <LoginForm />
+            {transition.map(({ item, key, props }) => (
+              <animated.div key={key} style={props}>
+                {item}
+              </animated.div>
+            ))}
           </Col>
           <Col xs={3} />
         </Row>
