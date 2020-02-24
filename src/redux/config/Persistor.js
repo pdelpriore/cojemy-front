a5aee2965d0d329a66a83623919257640f4ba786
pdelpriore <paul.del.priore29@gmail.com>
2020-02-24 15:31:33 +0100

    configure redux

---
+++ src/redux/config/Persistor.js
@@ -0,0 +1,23 @@
+import { createStore, compose, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import allReducers from "./combineReducers/Index";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "jE1NjAwMjQ1N",
+  storage
+};
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const persistedReducers = persistReducer(persistConfig, allReducers);
+
+export default () => {
+  let myStore = createStore(
+    persistedReducers,
+    composeEnhancer(applyMiddleware(thunk))
+  );
+  let persistor = persistStore(myStore);
+  return { myStore, persistor };
+};
