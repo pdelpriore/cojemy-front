6d6b52f09f221ff454ea52a92d5e39b9f4f32144
pdelpriore <paul.del.priore29@gmail.com>
2020-02-25 09:38:18 +0100

    change variable's name

--- src/redux/signup/thunk/SignupThunk.js
+++ src/redux/signup/thunk/SignupThunk.js
@@ -5,15 +5,14 @@ import { signupQuery } from "../query/signupQuery";
 export const signupUser = (name, email, confirmEmail, password) => {
   return async (dispatch, getState) => {
     dispatch({ type: signupCases.LOADING, payload: true });
-    const body = signupQuery(name, email, confirmEmail, password);
-    console.log(body);
+    const bodyRequest = signupQuery(name, email, confirmEmail, password);
     try {
       const response = await fetch(strings.path.SERVER_REQUEST, {
         method: "post",
         headers: {
           "Content-Type": "application/json"
         },
-        body: JSON.stringify(body)
+        body: JSON.stringify(bodyRequest)
       });
       const data = await response.json();
       console.log(data);
