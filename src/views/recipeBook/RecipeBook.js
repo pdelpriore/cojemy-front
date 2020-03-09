9d18a6cebe322e3a6177a4c9d86eec7a5c344727
pdelpriore <paul.del.priore29@gmail.com>
2020-03-09 14:49:25 +0100

    add recipe book page styles

--- src/views/recipeBook/RecipeBook.js
+++ src/views/recipeBook/RecipeBook.js
@@ -1,11 +1,27 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
+import { Row, Col, Image } from "react-bootstrap";
+import "./recipeBook.css";
 
 const RecipeBook = ({ match: { path, url, isExact } }) => {
   return (
-    <div>
+    <div className="recipebook-area">
       <Navbar path={path} url={url} isExact={isExact} />
-      Welcome to Recipe Book
+      <div>
+        <Row>
+          <Col xs={1} />
+          <Col xs={10}>
+            <div className="recipebook-center-area">
+              <div className="recipebook-list"></div>
+              <Image
+                className="recipebook-spoon"
+                src={require("../../assets/imgs/spoonret.jpg")}
+              />
+            </div>
+          </Col>
+          <Col xs={1} />
+        </Row>
+      </div>
     </div>
   );
 };
