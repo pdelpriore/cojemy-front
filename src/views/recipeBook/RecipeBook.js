262eaa185b184fed25d8857f6ab801324340e2b8
pdelpriore <paul.del.priore29@gmail.com>
2020-03-10 16:42:03 +0100

    add map buttons

--- src/views/recipeBook/RecipeBook.js
+++ src/views/recipeBook/RecipeBook.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import { Row, Col, Button, Image } from "react-bootstrap";
 import { capitalizeFirst } from "../../util/Util";
@@ -6,6 +6,26 @@ import { strings } from "../../strings/Strings";
 import "./recipeBook.css";
 
 const RecipeBook = ({ match: { path, url, isExact } }) => {
+  const [activesClasses, setActive] = useState([false, false]);
+
+  const toggleActiveClass = id => {
+    console.log(activesClasses[id]);
+    setActive(activesClasses => [
+      ...activesClasses,
+      (activesClasses[id] = !activesClasses[id])
+    ]);
+  };
+
+  const buttonItems = [
+    {
+      id: 1,
+      name: capitalizeFirst(strings.recipeBook.BUTTON_NEW)
+    },
+    {
+      id: 2,
+      name: capitalizeFirst(strings.recipeBook.BUTTON_FAST_FOOD)
+    }
+  ];
   return (
     <div className="recipebook-area">
       <Navbar path={path} url={url} isExact={isExact} />
@@ -21,30 +41,28 @@ const RecipeBook = ({ match: { path, url, isExact } }) => {
         <Row className="mb-5" />
         <Row className="mb-5" />
         <Row className="mb-5" />
-        <Row>
-          <Col xs={1} />
-          <Col xs={11}>
-            <Button variant="outline-dark" className="recipebook-knob-button">
-              <div className="recipe-button-icon" />
-              <div className="recipe-button-text">
-                {capitalizeFirst(strings.recipeBook.BUTTON_NEW)}
-              </div>
-            </Button>
-          </Col>
-        </Row>
-        <Row className="mb-3" />
-        <Row>
-          <Col xs={1} />
-          <Col xs={11}>
-            <Button variant="outline-dark" className="recipebook-knob-button">
-              <div className="recipe-button-icon" />
-              <div className="recipe-button-text">
-                {capitalizeFirst(strings.recipeBook.BUTTON_FAST_FOOD)}
-              </div>
-            </Button>
-          </Col>
-        </Row>
-        <Row className="mb-5" />
+        {buttonItems.map(buttonItem => (
+          <div key={buttonItem.id}>
+            <Row>
+              <Col xs={1} />
+              <Col xs={11}>
+                <Button
+                  onClick={() => toggleActiveClass(buttonItem.id)}
+                  variant="outline-dark"
+                  className={
+                    activesClasses[buttonItem.id]
+                      ? "recipe-button-active"
+                      : "recipebook-knob-button"
+                  }
+                >
+                  <div className="recipe-button-icon" />
+                  <div className="recipe-button-text">{buttonItem.name}</div>
+                </Button>
+              </Col>
+            </Row>
+            <Row className="mb-3" />
+          </div>
+        ))}
         <Row className="mb-5" />
         <Row className="mb-5" />
         <Row>
