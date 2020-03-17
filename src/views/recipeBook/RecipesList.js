c4df9ce9f581f028a22e45eb54328f5176bd5f9b
pdelpriore <paul.del.priore29@gmail.com>
2020-03-17 15:57:09 +0100

    design item recipe list

--- src/views/recipeBook/RecipesList.js
+++ src/views/recipeBook/RecipesList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import SimpleBar from "simplebar-react";
-import { Spinner } from "react-bootstrap";
+import { Row, Col, Image, Spinner } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import "./recipeBook.css";
 
@@ -19,7 +19,48 @@ const RecipesList = () => {
       ) : (
         <SimpleBar>
           {recipesRetrieved.map((retrieveRecipe, index) => (
-            <div key={index}>{retrieveRecipe.title}</div>
+            <div className="recipesList-item" key={index}>
+              <Row className="mb-3" />
+              <Row>
+                <Col xs={1} />
+                <Col xs={3}>
+                  <Image
+                    className="recipesList-item-picture"
+                    src={
+                      retrieveRecipe.picture
+                        ? retrieveRecipe.picture
+                        : require("../../assets/imgs/panret.jpg")
+                    }
+                    roundedCircle
+                  />
+                </Col>
+                <Col xs={7}>
+                  <div style={{ height: 50 }} />
+                  <div className="recipesList-item-title">
+                    {retrieveRecipe.title}
+                  </div>
+                </Col>
+                <Col xs={1} />
+              </Row>
+              <Row className="mb-3" />
+              <Row>
+                <Col xs={1} />
+                <Col xs={11}>
+                  <div className="recipesList-item-author">
+                    par: typol jakis
+                  </div>
+                </Col>
+              </Row>
+              <Row className="mb-3" />
+              <Row>
+                <Col xs={1} />
+                <Col xs={11}>
+                  <div className="recipesList-item-time">
+                    temps: iles tam minut
+                  </div>
+                </Col>
+              </Row>
+            </div>
           ))}
         </SimpleBar>
       )}
