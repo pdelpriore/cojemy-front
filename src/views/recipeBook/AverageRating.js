56e20c131e3dc66e7b630d2421cc2735cd75c693
pdelpriore <paul.del.priore29@gmail.com>
2020-03-23 18:33:23 +0100

    prepare average rating

--- src/views/recipeBook/AverageRating.js
+++ src/views/recipeBook/AverageRating.js
@@ -3,23 +3,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import "./recipeBook.css";
 
-const Rate = ({ rate }) => {
+const AverageRating = ({ data }) => {
+  console.log(data);
   const stars = [];
   for (let i = 0; i < 5; i++)
     stars.push(
       <FontAwesomeIcon className="recipeDetails-comments-stars" icon={faStar} />
     );
 
-  for (let i = 0; i < rate; i++) {
-    stars[i] = (
-      <FontAwesomeIcon
-        className="recipeDetails-comments-stars-active"
-        icon={faStar}
-      />
-    );
-  }
+  const rates = data.map(item => item.rate.value);
+  const average = rates.reduce((a, b) => a + b) / rates.length;
+  const starPercentage = (average / 5) * 100;
+  const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+  console.log(starPercentageRounded);
 
   return stars.map((star, index) => <div key={index}>{star}</div>);
 };
 
-export default Rate;
+export default AverageRating;
--- src/views/recipeBook/RecipeDetails.js
+++ src/views/recipeBook/RecipeDetails.js
@@ -10,6 +10,7 @@ import fr from "timeago.js/lib/lang/fr";
 import { createDate } from "../../util/Util";
 import { useDispatch, useSelector } from "react-redux";
 import Rate from "./Rate";
+import AverageRating from "./AverageRating";
 import { recipeDetailsClearState } from "../../redux/showRecipeDetails/thunk/showRecipeDetailsThunk";
 import "./recipeBook.css";
 
@@ -19,116 +20,123 @@ const RecipeDetails = () => {
     state => state.showRecipeDetails
   );
   timeago.register("fr", fr);
-  console.log(detailsDataRetrieved);
   return (
-    <ScrollArea smoothScrolling={true} className="recipeDetails-main">
-      <div>
-        <Row>
-          <Col xs={10}>
-            <h1 className="recipeDetails-title">
-              {detailsDataRetrieved.title}
-            </h1>
-          </Col>
-          <Col xs={1}>
-            <div
-              onClick={e => {
-                e.preventDefault();
-                dispatch(recipeDetailsClearState());
-              }}
-              className="recipeDetails-close-icon"
-            >
-              <FontAwesomeIcon icon={faTimesCircle} />
-            </div>
-          </Col>
-          <Col xs={1} />
-        </Row>
-        <Row className="mb-2" />
-        <Row>
-          <Col xs={1} />
-          <Col xs={5}>
-            <Img
-              className="recipeDetails-picture"
-              src={
-                detailsDataRetrieved.picture
-                  ? detailsDataRetrieved.picture
-                  : require("../../assets/imgs/panret.jpg")
-              }
-              loader={<Spinner animation="border" variant="dark" />}
-            />
-          </Col>
-          <Col xs={5}>
-            <div className="recipeDetails-ingredients-text">Ingrédients:</div>
-            <div style={{ height: 10 }} />
-            {detailsDataRetrieved.ingredients.map((ingredient, index) => (
-              <div className="recipeDetails-ingredients-item" key={index}>
-                - {ingredient}
+    <>
+      <Row>
+        <Col xs={1} />
+        <Col xs={7}>
+          <h1 className="recipeDetails-title">{detailsDataRetrieved.title}</h1>
+        </Col>
+        <Col xs={2}>
+          <div className="recipeDetails-comments-rate">
+            <AverageRating data={detailsDataRetrieved.comments} />
+          </div>
+        </Col>
+        <Col xs={1}>
+          <div
+            onClick={e => {
+              e.preventDefault();
+              dispatch(recipeDetailsClearState());
+            }}
+            className="recipeDetails-close-icon"
+          >
+            <FontAwesomeIcon icon={faTimesCircle} />
+          </div>
+        </Col>
+        <Col xs={1} />
+      </Row>
+      <Row className="mb-2" />
+      <ScrollArea smoothScrolling={true} className="recipeDetails-main">
+        <div>
+          <Row>
+            <Col xs={1} />
+            <Col xs={5}>
+              <Img
+                className="recipeDetails-picture"
+                src={
+                  detailsDataRetrieved.picture
+                    ? detailsDataRetrieved.picture
+                    : require("../../assets/imgs/panret.jpg")
+                }
+                loader={<Spinner animation="border" variant="dark" />}
+              />
+            </Col>
+            <Col xs={5}>
+              <div className="recipeDetails-ingredients-text">Ingrédients:</div>
+              <div style={{ height: 10 }} />
+              {detailsDataRetrieved.ingredients.map((ingredient, index) => (
+                <div className="recipeDetails-ingredients-item" key={index}>
+                  - {ingredient}
+                </div>
+              ))}
+            </Col>
+            <Col xs={1} />
+          </Row>
+          <Row className="mb-3" />
+          <Row>
+            <Col xs={1} />
+            <Col xs={10}>
+              <div className="recipeDetails-ingredients-text">
+                Description:{" "}
               </div>
-            ))}
-          </Col>
-          <Col xs={1} />
-        </Row>
-        <Row className="mb-3" />
-        <Row>
-          <Col xs={1} />
-          <Col xs={10}>
-            <div className="recipeDetails-ingredients-text">Description: </div>
-            <div style={{ height: 10 }} />
-            <div className="recipeDetails-ingredients-description">
-              {detailsDataRetrieved.description}
-            </div>
-          </Col>
-          <Col xs={1} />
-        </Row>
-        <Row className="mb-5" />
-        <Row>
-          <Col xs={1} />
-          <Col xs={10}>
-            {detailsDataRetrieved.comments.map((item, index) => (
-              <div className="recipeDetails-comments-area" key={index}>
-                {item.commentator.photo || item.commentator.googlePhoto ? (
-                  <Img
-                    className="recipeDetails-comments-photo"
-                    src={
-                      item.commentator.photo
-                        ? item.commentator.photo
-                        : item.commentator.googlePhoto
-                        ? item.commentator.googlePhoto
-                        : null
-                    }
-                    loader={
-                      <Spinner animation="border" size="sm" variant="dark" />
-                    }
-                  />
-                ) : (
-                  <FontAwesomeIcon
-                    className="recipeDetails-comments-icon"
-                    icon={faUser}
-                  />
-                )}
-                <div className="recipeDetails-comments-content-area">
-                  <div className="recipeDetails-comments-author">
-                    {item.commentator.name}
-                    <TimeAgo
-                      className="recipeDetails-comments-timeago"
-                      datetime={createDate(item.comment.date)}
-                      locale="fr"
+              <div style={{ height: 10 }} />
+              <div className="recipeDetails-ingredients-description">
+                {detailsDataRetrieved.description}
+              </div>
+            </Col>
+            <Col xs={1} />
+          </Row>
+          <Row className="mb-5" />
+          <Row>
+            <Col xs={1} />
+            <Col xs={10}>
+              {detailsDataRetrieved.comments.map((item, index) => (
+                <div className="recipeDetails-comments-area" key={index}>
+                  {item.commentator.photo || item.commentator.googlePhoto ? (
+                    <Img
+                      className="recipeDetails-comments-photo"
+                      src={
+                        item.commentator.photo
+                          ? item.commentator.photo
+                          : item.commentator.googlePhoto
+                          ? item.commentator.googlePhoto
+                          : null
+                      }
+                      loader={
+                        <Spinner animation="border" size="sm" variant="dark" />
+                      }
                     />
-                  </div>
-                  <div className="recipeDetails-comments-rate">
-                    <Rate rate={item.rate.value} />
-                  </div>
-                  <div style={{ height: 5 }} />
-                  <div className="recipeDetails-comments-content">
-                    {item.comment.content}
+                  ) : (
+                    <FontAwesomeIcon
+                      className="recipeDetails-comments-icon"
+                      icon={faUser}
+                    />
+                  )}
+                  <div className="recipeDetails-comments-content-area">
+                    <div className="recipeDetails-comments-author">
+                      {item.commentator.name}
+                      <TimeAgo
+                        className="recipeDetails-comments-timeago"
+                        datetime={createDate(item.comment.date)}
+                        locale="fr"
+                      />
+                    </div>
+                    <div className="recipeDetails-comments-rate">
+                      <Rate rate={item.rate.value} />
+                    </div>
+                    <div style={{ height: 5 }} />
+                    <div className="recipeDetails-comments-content">
+                      {item.comment.content}
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </Col>
-          <Col xs={1} />
-        </Row>
-      </div>
-    </ScrollArea>
+              ))}
+            </Col>
+            <Col xs={1} />
+          </Row>
+        </div>
+      </ScrollArea>
+    </>
   );
 };
 
