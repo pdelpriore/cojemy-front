9f2bcdd2d18fb7f2a239f2d10e9c72b2f95a765e
pdelpriore <paul.del.priore29@gmail.com>
2020-03-23 18:42:20 +0100

    null protection

--- src/views/recipeBook/AverageRating.js
+++ src/views/recipeBook/AverageRating.js
@@ -12,7 +12,8 @@ const AverageRating = ({ data }) => {
     );
 
   const rates = data.map(item => item.rate.value);
-  const average = rates.reduce((a, b) => a + b) / rates.length;
+  const average =
+    rates.length > 0 && rates.reduce((a, b) => a + b) / rates.length;
   const starPercentageRounded = `${Math.round(((average / 5) * 100) / 10) *
     10}%`;
   console.log(starPercentageRounded);
