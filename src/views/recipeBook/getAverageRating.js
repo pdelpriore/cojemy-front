5cf6cb508ae00bd29740d566e0115ea53f278717
pdelpriore <paul.del.priore29@gmail.com>
2020-03-30 09:43:26 +0200

    average rating corrections

--- src/views/recipeBook/getAverageRating.js
+++ src/views/recipeBook/getAverageRating.js
@@ -1,9 +1,9 @@
 export const getAverageRating = data => {
   const rates = data.map(item => item.rate.value);
   const average =
-    rates.length > 0 && rates.reduce((a, b) => a + b) / rates.length;
-  const starPercentageRounded = `${Math.round(((average / 5) * 100) / 10) *
-    10}%`;
+    rates.length > 0 &&
+    (rates.reduce((a, b) => a + b) / rates.length).toFixed(2);
+  const starPercentageRounded = `${((average * 100) / 5).toFixed()}%`;
 
   return starPercentageRounded;
 };
