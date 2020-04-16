0cb59a83fe0083341ed7cba3a66774aa6494b1d0
pdelpriore <paul.del.priore29@gmail.com>
2020-04-16 14:34:24 +0200

    add capitalize first in title and description input

--- src/hooks/screen/myRecipes/useNewRecipeForm.js
+++ src/hooks/screen/myRecipes/useNewRecipeForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { capitalizeFirst } from "../../../util/Util";
 
 const useNewRecipeForm = () => {
   const [inputs, setInputs] = useState({});
@@ -8,10 +9,14 @@ const useNewRecipeForm = () => {
     setInputs((inputs) => ({
       ...inputs,
       [e.target.name]:
-        e.target.name === "ingredients"
+        e.target.name === "title"
+          ? capitalizeFirst(e.target.value)
+          : e.target.name === "ingredients"
           ? e.target.value.replace(/[^a-zA-Z,\d\s]/g, "").split(/\s*,\s*/)
           : e.target.name === "cookTime"
           ? e.target.value.replace(/[^1-9]+/g, "")
+          : e.target.name === "description"
+          ? capitalizeFirst(e.target.value)
           : e.target.value,
     }));
   };
