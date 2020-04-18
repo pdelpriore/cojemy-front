99867816f0666a6dc1b22fdf7f340b6bfc7b5ec3
pdelpriore <paul.del.priore29@gmail.com>
2020-04-18 11:35:13 +0200

    refactor add new recipe query

--- src/redux/myRecipes/retrieveMyRecipes/query/addMyRecipeQuery.js
+++ src/redux/myRecipes/retrieveMyRecipes/query/addMyRecipeQuery.js
@@ -9,44 +9,52 @@ export const addMyRecipeQuery = (
   email
 ) => {
   return {
-    query: `
-                  mutation {
-                    addMyRecipe(title: "${title}", image: "${image}", video: "${video}", category: "${category}", cookTime: ${cookTime}, ingredients: ["${ingredients}"], description: "${description}", email: "${email}") {
-                        _id
-                        title
-                        picture
-                        video
-                        date
-                        category
-                        cookTime
-                        ingredients
-                        description
-                        author {
-                            name
-                            photo
-                            googlePhoto
-                        }
-                        comments {
-                          _id
-                          commentator {
-                            _id
-                            name
-                            email
-                            photo
-                            googlePhoto
-                          }
-                          comment {
-                            _id
-                            content
-                            date
-                          }
-                          rate {
-                            _id
-                            value
-                          }
-                        }
-                    }
-                  }
-                  `,
+    query: `mutation($title: String!, $image: String, $video: String, $category: String!, $cookTime: Int!, $ingredients: [String!]!, $description: String!, $email: String!) {
+    addMyRecipe(title: $title, image: $image, video: $video, category: $category, cookTime: $cookTime, ingredients: $ingredients, description: $description, email: $email) {
+        _id
+        title
+        picture
+        video
+        date
+        category
+        cookTime
+        ingredients
+        description
+        author {
+            name
+            photo
+            googlePhoto
+        }
+        comments {
+          _id
+          commentator {
+            _id
+            name
+            email
+            photo
+            googlePhoto
+          }
+          comment {
+            _id
+            content
+            date
+          }
+          rate {
+            _id
+            value
+          }
+        }
+    }
+  }`,
+    variables: {
+      title,
+      image,
+      video,
+      category,
+      cookTime,
+      ingredients,
+      description,
+      email,
+    },
   };
 };
