7f6c526742b6eb197c16f553fdcf3795f088f8fc
pdelpriore <paul.del.priore29@gmail.com>
2020-04-27 17:25:42 +0200

    avoid to set data undefined in database when edit my recipe

--- src/hooks/form/myRecipes/useNewRecipeForm.js
+++ src/hooks/form/myRecipes/useNewRecipeForm.js
@@ -128,7 +128,7 @@ const useNewRecipeForm = () => {
           inputs.title,
           inputs.recipeImage.image,
           inputs.recipeImage.imageName,
-          inputs.video,
+          inputs.video === undefined ? "" : inputs.video,
           inputs.category,
           parseInt(inputs.cookTime),
           inputs.ingredients.filter((ingredient) => ingredient !== ""),
