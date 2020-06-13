3ab043580ec46d51544cff34daf7889b54b31a00
pdelpriore <paul.del.priore29@gmail.com>
2020-06-13 10:55:43 +0200

    small refactor of sort function

--- src/shared/sortCommentsByDate.js
+++ src/shared/sortCommentsByDate.js
@@ -1,5 +1,5 @@
 export const sortCommentsByDate = (data) => {
-  return data.comments.sort((a, b) => {
-    return a.comment.date > b.comment.date ? -1 : 1;
-  });
+  return data.comments.sort((a, b) =>
+    a.comment.date > b.comment.date ? -1 : 1
+  );
 };
