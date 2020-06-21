1062520713c1d4bb4627e89b3980426d7c50df7a
pdelpriore <paul.del.priore29@gmail.com>
2020-06-21 15:12:09 +0200

    add a little map style

--- src/components/map/map/Map.js
+++ src/components/map/map/Map.js
@@ -7,34 +7,36 @@ import "./map.css";
 const Map = ({ latitude, longitude, zoom }) => {
   const { showLoading, handleHideLoading } = useMap();
   return (
-    <Row>
-      <Col xs={12}>
-        <div className="map-iframe-box">
-          {showLoading && (
-            <div className="map-iframe-spinner-box">
-              <Row>
-                <Col xs={4} />
-                <Col xs={4}>
-                  <Spinner animation="border" size="sm" />
-                </Col>
-                <Col xs={4} />
-              </Row>
-            </div>
-          )}
-          <iframe
-            title="map"
-            className={showLoading ? "map-iframe-hidden" : ""}
-            style={{ height: "450px" }}
-            src={
-              strings.path.MAP_REQUEST + `/${latitude}/${longitude}/${zoom}/`
-            }
-            scrolling="no"
-            frameBorder="0"
-            onLoad={handleHideLoading}
-          />
-        </div>
-      </Col>
-    </Row>
+    <div className="map-iframe-main-area">
+      <Row>
+        <Col xs={12}>
+          <div className="map-iframe-box">
+            {showLoading && (
+              <div className="map-iframe-spinner-box">
+                <Row>
+                  <Col xs={4} />
+                  <Col xs={4}>
+                    <Spinner animation="border" size="sm" />
+                  </Col>
+                  <Col xs={4} />
+                </Row>
+              </div>
+            )}
+            <iframe
+              title="map"
+              className={showLoading ? "map-iframe-hidden" : ""}
+              style={{ height: "450px" }}
+              src={
+                strings.path.MAP_REQUEST + `/${latitude}/${longitude}/${zoom}/`
+              }
+              scrolling="no"
+              frameBorder="0"
+              onLoad={handleHideLoading}
+            />
+          </div>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
