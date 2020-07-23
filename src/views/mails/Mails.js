57754360cd384005ec95c8d24f344e84af70d2ec
pdelpriore <paul.del.priore29@gmail.com>
2020-07-23 13:02:46 +0200

    select socket in mail component

--- src/views/mails/Mails.js
+++ src/views/mails/Mails.js
@@ -3,9 +3,7 @@ import Navbar from "../../components/navbar/Navbar";
 import { useSelector } from "react-redux";
 
 const Mails = ({ match: { path, url, isExact } }) => {
-  const {
-    userSocketData: { userId, userSocketId, socket },
-  } = useSelector((state) => state.socketData);
+  const { ioSocket } = useSelector((state) => state.socket);
   return (
     <div>
       <Navbar path={path} url={url} isExact={isExact} />
