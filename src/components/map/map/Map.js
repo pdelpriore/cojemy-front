d14d62a82e5a02d05472d62f362a987a9a7f1167
pdelpriore <paul.del.priore29@gmail.com>
2020-07-26 15:31:13 +0200

    check user auth on render map

--- src/components/map/map/Map.js
+++ src/components/map/map/Map.js
@@ -1,11 +1,13 @@
 import React from "react";
 import useMap from "../../../hooks/screen/myEvents/useMap";
+import { useSelector } from "react-redux";
 import { Row, Col, Spinner } from "react-bootstrap";
 import { strings } from "../../../strings/Strings";
 import "./map.css";
 
 const Map = ({ latitude, longitude, zoom }) => {
   const { showLoading, handleHideLoading } = useMap();
+  const { userData } = useSelector((state) => state.login);
   return (
     <div className="map-iframe-main-area">
       <Row>
@@ -27,7 +29,8 @@ const Map = ({ latitude, longitude, zoom }) => {
               className={showLoading ? "map-iframe-hidden" : ""}
               style={{ height: "450px" }}
               src={
-                strings.path.MAP_REQUEST + `/${latitude}/${longitude}/${zoom}/`
+                strings.path.MAP_REQUEST +
+                `/${latitude}/${longitude}/${zoom}/${userData._id}/${userData.email}`
               }
               scrolling="no"
               frameBorder="0"
