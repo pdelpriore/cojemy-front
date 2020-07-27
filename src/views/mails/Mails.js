0254f596e68edaca8b7c6bf2da7785e3bd6805bc
pdelpriore <paul.del.priore29@gmail.com>
2020-07-27 11:16:22 +0200

    prepare mails component styles

--- src/views/mails/Mails.js
+++ src/views/mails/Mails.js
@@ -1,15 +1,39 @@
 import React, { useEffect } from "react";
 import Navbar from "../../components/navbar/Navbar";
+import { Row, Col, Button, ListGroup, Image } from "react-bootstrap";
+import ScrollArea from "react-scrollbar";
+import { useSpring, animated } from "react-spring";
 import { useSelector } from "react-redux";
+import "./mails.css";
 
 const Mails = ({ match: { path, url, isExact } }) => {
   const { socket } = useSelector((state) => state.socketData);
 
+  const props = useSpring({
+    opacity: 1,
+    config: { duration: 200 },
+    from: { opacity: 0 },
+  });
   return (
-    <div>
+    <animated.div className="mails-area" style={props}>
       <Navbar path={path} url={url} isExact={isExact} />
-      Welcome to Mails
-    </div>
+      <div className="mails-first-section">
+        <Row>
+          <Col xs={3} />
+          <Col xs={9}>
+            <div className="myrecipes-list">
+              <ScrollArea
+                className="myrecipes-scroll-area"
+                smoothScrolling={true}
+              >
+                <ListGroup></ListGroup>
+              </ScrollArea>
+            </div>
+          </Col>
+        </Row>
+      </div>
+      <div className="mails-second-section"></div>
+    </animated.div>
   );
 };
 
