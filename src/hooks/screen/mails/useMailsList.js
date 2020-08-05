b22e1a7b7ca18f5f10046dbfd9d0a511bac94139
pdelpriore <paul.del.priore29@gmail.com>
2020-08-05 13:49:31 +0200

    implement receiving messages

--- src/hooks/screen/mails/useMailsList.js
+++ src/hooks/screen/mails/useMailsList.js
@@ -16,8 +16,28 @@ const useMailsList = () => {
   }, []);
 
   useEffect(() => {
-    if (socket.connected && isActive) {
+    if (socket.connected && messages.length === 0 && isActive) {
+      setLoading(true);
       socket.emit("getMessages", userData._id);
+      socket.on("messagesRetrieved", (data) => {
+        if (data) {
+          if (error.getMessagesError) {
+            setError({});
+          }
+          setLoading(false);
+          // dispatchuj do reduxa wszystkie wiadomosci, zeby nie zniknely po odswiezeniu
+          setMessages(data);
+        }
+      });
+      socket.on("getMessagesError", (err) => {
+        if (err) {
+          setError((error) => ({
+            ...error,
+            getMessagesError: err,
+          }));
+          setLoading(false);
+        }
+      });
     }
     return () => {
       if (
@@ -25,10 +45,13 @@ const useMailsList = () => {
         (messages.length > 0 || messages.length === 0) &&
         isActive
       ) {
-        socket.removeAllListeners("getMessages");
+        socket.removeAllListeners("messagesRetrieved");
+        socket.removeAllListeners("getMessagesError");
       }
     };
-  }, [socket, messages, isActive, userData._id]);
+  }, [socket, messages, isActive, userData._id, error.getMessagesError]);
+
+  console.log(messages);
 
   return { loading, messages, error };
 };
