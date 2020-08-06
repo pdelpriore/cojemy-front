ff5414d717ff8632c13b0799c5bd116561753f0f
pdelpriore <paul.del.priore29@gmail.com>
2020-08-06 10:53:53 +0200

    show messages in the list

--- src/views/mails/MailsList.js
+++ src/views/mails/MailsList.js
@@ -12,57 +12,58 @@ import "./mails.css";
 const MailsList = () => {
   const dispatch = useDispatch();
 
-  const { loading, messages, error } = useMailsList();
+  const { loading, error } = useMailsList();
+  const { messages } = useSelector((state) => state.userMessages);
 
-  return <div>brak wiadomosci</div>;
-
-  //   return loading ? (
-  //     <div className="myrecipes-list-loading-area">
-  //       <Spinner animation="border" role="status" variant="light" />
-  //     </div>
-  //   ) : error ? (
-  //     <div className="myrecipes-list-item-norecipes">{error}</div>
-  //   ) : (
-  //     <div className="myrecipes-list-main-area">
-  //       {messages.length > 0 &&
-  //         messages.map((message, index) => (
-  //           <div
-  //             onClick={(e) => {
-  //               e.preventDefault();
-  //               //dispatch(myRecipeData(retrieveRecipe));
-  //             }}
-  //             className="myrecipes-list-item"
-  //             key={index}
-  //           >
-  //             <Row>
-  //               <Col xs={3}>
-  //                 <Img
-  //                   className="myrecipes-list-item-picture"
-  //                   src={require("../../assets/imgs/panret.jpg")}
-  //                   loader={<Spinner animation="border" variant="info" />}
-  //                 />
-  //               </Col>
-  //               <Col xs={9}>
-  //                 <Row>
-  //                   <Col xs={7} />
-  //                   <Col xs={5}>
-  //                     <div>data</div>
-  //                   </Col>
-  //                 </Row>
-  //                 <div className="myrecipes-list-item-title">recipient name</div>
-  //                 <div style={{ height: 5 }} />
-  //                 <div className="myrecipes-list-item-author">
-  //                   <div className="myrecipes-list-item-icon">
-  //                     <FontAwesomeIcon icon={faEnvelope} />
-  //                   </div>
-  //                   <div>tresc ostatniej wiadomosci</div>
-  //                 </div>
-  //               </Col>
-  //             </Row>
-  //           </div>
-  //         ))}
-  //     </div>
-  //   );
+  return loading ? (
+    <div className="myrecipes-list-loading-area">
+      <Spinner animation="border" role="status" variant="light" />
+    </div>
+  ) : error.getMessagesError ? (
+    <div className="myrecipes-list-item-norecipes">
+      {error.getMessagesError}
+    </div>
+  ) : (
+    <div className="myrecipes-list-main-area">
+      {messages.length > 0 &&
+        messages.map((message, index) => (
+          <div
+            onClick={(e) => {
+              e.preventDefault();
+              //dispatch(myRecipeData(retrieveRecipe));
+            }}
+            className="myrecipes-list-item"
+            key={index}
+          >
+            <Row>
+              <Col xs={3}>
+                <Img
+                  className="mails-list-item-picture"
+                  src={require("../../assets/imgs/cookerret.png")}
+                  loader={<Spinner animation="border" variant="info" />}
+                />
+              </Col>
+              <Col xs={9}>
+                <Row>
+                  <Col xs={7} />
+                  <Col xs={5}>
+                    <div>data</div>
+                  </Col>
+                </Row>
+                <div className="myrecipes-list-item-title">recipient name</div>
+                <div style={{ height: 5 }} />
+                <div className="myrecipes-list-item-author">
+                  <div className="myrecipes-list-item-icon">
+                    <FontAwesomeIcon icon={faEnvelope} />
+                  </div>
+                  <div>tresc ostatniej wiadomosci</div>
+                </div>
+              </Col>
+            </Row>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default MailsList;
