b43c9e746eb4739a9d1a2bbc76ab521e2875d163
pdelpriore <paul.del.priore29@gmail.com>
2020-08-22 23:45:16 +0200

    refactor make recipe buttons

--- src/hooks/screen/recipeBook/useRecipeButton.js
+++ src/hooks/screen/recipeBook/useRecipeButton.js
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { getRecipe } from "../../../redux/recipeBook/retrieveRecipe/thunk/retrieveRecipesThunk";
 import { recipeButtonTurnedOn } from "../../../redux/recipeBook/recipeButtonTurnedOn/thunk/recipeButtonTurnedOnThunk";
 import { categorySelected } from "../../../redux/recipeBook/recipeCategorySelected/thunk/recipeCategorySelectedThunk";
+import { changeRateComment } from "../../../redux/recipeBook/changeRateComment/thunk/changeRateCommentThunk";
+import { recipeButtonItemsArray } from "../../../shared/buttonItemsArray";
 
 const useRecipeButton = (buttonQty) => {
   const dispatch = useDispatch();
@@ -19,6 +21,18 @@ const useRecipeButton = (buttonQty) => {
   const [buttonPressedCount, setButtonPressedCount] = useState(0);
 
   const { userData } = useSelector((state) => state.login);
+  const { recipeButtonId } = useSelector(
+    (state) => state.recipeCategorySelected
+  );
+  const { rateCommentChanged } = useSelector(
+    (state) => state.isRateCommentChanged
+  );
+  const { detailsDataRetrieved } = useSelector(
+    (state) => state.isRecipeDetailsShown
+  );
+  const { searchInputFilled } = useSelector(
+    (state) => state.turnOffRecipeButtons
+  );
 
   const toggleActiveClass = (id, category) => {
     const skip = 1;
@@ -45,6 +59,28 @@ const useRecipeButton = (buttonQty) => {
   useEffect(() => {
     dispatch(recipeButtonTurnedOn(buttonPressedCount));
   }, [buttonPressedCount, dispatch]);
+
+  useEffect(() => {
+    if (!detailsDataRetrieved.title && !searchInputFilled)
+      toggleActiveClass(
+        recipeButtonItemsArray[recipeButtonId].id,
+        recipeButtonItemsArray[recipeButtonId].category
+      );
+  }, [detailsDataRetrieved, recipeButtonId, searchInputFilled]);
+
+  useEffect(() => {
+    if (searchInputFilled) {
+      toggleActiveClass(null, null);
+    } else {
+      toggleActiveClass(
+        recipeButtonItemsArray[recipeButtonId].id,
+        recipeButtonItemsArray[recipeButtonId].category
+      );
+    }
+    return () => {
+      dispatch(changeRateComment(false));
+    };
+  }, [searchInputFilled, recipeButtonId, rateCommentChanged, dispatch]);
 
   return {
     activesClasses,
--- src/views/recipeBook/makeRecipeButton.js
+++ src/views/recipeBook/makeRecipeButton.js
@@ -1,54 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Row, Col, Button } from "react-bootstrap";
 import useRecipeButton from "../../hooks/screen/recipeBook/useRecipeButton";
-import { changeRateComment } from "../../redux/recipeBook/changeRateComment/thunk/changeRateCommentThunk";
-import { useSelector, useDispatch } from "react-redux";
 import { capitalize } from "../../util/Util";
 import { recipeButtonItemsArray } from "../../shared/buttonItemsArray";
 import "./recipeBook.css";
 
 const MakeRecipeButton = () => {
-  const dispatch = useDispatch();
-  const { recipeButtonId } = useSelector(
-    (state) => state.recipeCategorySelected
-  );
-  const { rateCommentChanged } = useSelector(
-    (state) => state.isRateCommentChanged
-  );
-  const { detailsDataRetrieved } = useSelector(
-    (state) => state.isRecipeDetailsShown
-  );
-  const { searchInputFilled } = useSelector(
-    (state) => state.turnOffRecipeButtons
-  );
   const {
     activesClasses,
     toggleActiveClass,
     buttonPressedCount,
     setButtonPressedCount,
   } = useRecipeButton(recipeButtonItemsArray.length);
-
-  useEffect(() => {
-    if (!detailsDataRetrieved.title && !searchInputFilled)
-      toggleActiveClass(
-        recipeButtonItemsArray[recipeButtonId].id,
-        recipeButtonItemsArray[recipeButtonId].category
-      );
-  }, [detailsDataRetrieved, recipeButtonId, searchInputFilled]);
-
-  useEffect(() => {
-    if (searchInputFilled) {
-      toggleActiveClass(null, null);
-    } else {
-      toggleActiveClass(
-        recipeButtonItemsArray[recipeButtonId].id,
-        recipeButtonItemsArray[recipeButtonId].category
-      );
-    }
-    return () => {
-      dispatch(changeRateComment(false));
-    };
-  }, [searchInputFilled, recipeButtonId, rateCommentChanged, dispatch]);
 
   return recipeButtonItemsArray.map((buttonItem) => (
     <div key={buttonItem.id}>
