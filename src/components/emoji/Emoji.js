3ba79d46764aad97c5771356d515e3ea9aa408f9
pdelpriore <paul.del.priore29@gmail.com>
2020-09-08 15:01:57 +0200

    prepare emojis category

--- src/components/emoji/Emoji.js
+++ src/components/emoji/Emoji.js
@@ -1,10 +1,47 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 import useEmoji from "../../hooks/screen/emoji/useEmoji";
+import { useSelector, useDispatch } from "react-redux";
 import "./emoji.css";
 
 const Emoji = () => {
-  const { emojis } = useEmoji();
-  return <div></div>;
+  const dispatch = useDispatch();
+  const {
+    emojis,
+    categories,
+    categoryIndex,
+    handleSelectCategory,
+  } = useEmoji();
+
+  const { emojiLoading, emojisAll } = useSelector((state) => state.emojis);
+
+  return (
+    <div className="emoji-box">
+      <Row>
+        <Col xs={12}>
+          <div className="emoji-category-names">
+            {categories.length > 0 &&
+              categories.map((category, index) => (
+                <div
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleSelectCategory(index);
+                  }}
+                  className={
+                    categoryIndex === index
+                      ? "emoji-category-active"
+                      : "emoji-category"
+                  }
+                  key={index}
+                >
+                  {category}
+                </div>
+              ))}
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Emoji;
--- src/hooks/screen/emoji/useEmoji.js
+++ src/hooks/screen/emoji/useEmoji.js
@@ -1,17 +1,49 @@
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getEmojis } from "../../../redux/emoji/getEmojis/thunk/getEmojisThunk";
+import { strings } from "../../../strings/Strings";
 
 const useEmoji = () => {
   const dispatch = useDispatch();
 
+  const categories = [
+    strings.emojis.categories.EMOTIONS,
+    strings.emojis.categories.BODY,
+    strings.emojis.categories.COMPONENT,
+    strings.emojis.categories.ANIMALS,
+    strings.emojis.categories.FOOD,
+    strings.emojis.categories.TRAVEL,
+    strings.emojis.categories.ACTIVITIES,
+    strings.emojis.categories.OBJECTS,
+    strings.emojis.categories.SYMBOLS,
+    strings.emojis.categories.FLAGS,
+  ];
+
+  const categories_eng = [
+    strings.emojis.categories_eng.EMOTIONS,
+    strings.emojis.categories_eng.BODY,
+    strings.emojis.categories_eng.COMPONENT,
+    strings.emojis.categories_eng.ANIMALS,
+    strings.emojis.categories_eng.FOOD,
+    strings.emojis.categories_eng.TRAVEL,
+    strings.emojis.categories_eng.ACTIVITIES,
+    strings.emojis.categories_eng.OBJECTS,
+    strings.emojis.categories_eng.SYMBOLS,
+    strings.emojis.categories_eng.FLAGS,
+  ];
+
   const [emojis, setEmojis] = useState(null);
+  const [categoryIndex, setCategoryIndex] = useState(0);
+
+  const handleSelectCategory = (catIndex) => {
+    setCategoryIndex(catIndex);
+  };
 
   useEffect(() => {
     dispatch(getEmojis());
   }, []);
 
-  return { emojis };
+  return { emojis, categories, categoryIndex, handleSelectCategory };
 };
 
 export default useEmoji;
