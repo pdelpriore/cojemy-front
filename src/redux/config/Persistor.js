7e74fb43a58fbe4b37d276a2e5b6f072bb38b434
pdelpriore <paul.del.priore29@gmail.com>
2020-09-16 14:46:22 +0200

    disabled redux store preview

--- src/redux/config/Persistor.js
+++ src/redux/config/Persistor.js
@@ -1,4 +1,4 @@
-import { createStore, compose, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import encryptor from "./Encryptor";
 import storage from "redux-persist/lib/storage";
@@ -11,15 +11,10 @@ const persistConfig = {
   transforms: [encryptor],
 };
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const persistedReducers = persistReducer(persistConfig, allReducers);
 
 export default () => {
-  let myStore = createStore(
-    persistedReducers,
-    composeEnhancer(applyMiddleware(thunk))
-  );
+  let myStore = createStore(persistedReducers, applyMiddleware(thunk));
   let persistor = persistStore(myStore);
   return { myStore, persistor };
 };
