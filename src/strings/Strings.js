0c2f6ff9dcd5b7ffdf79d0f08e57765b17b9a308
pdelpriore <paul.del.priore29@gmail.com>
2020-09-16 17:41:29 +0200

    change for https

--- src/strings/Strings.js
+++ src/strings/Strings.js
@@ -1,7 +1,7 @@
 export const strings = Object.freeze({
   path: {
     HOME: "/",
-    GRAPHQL: "http://localhost:4000/graphql",
+    GRAPHQL: "https://localhost:4000/graphql",
     LOGIN: "/logowanie",
     SIGNUP: "/rejestracja",
     SIGNOUT: "/wylogowanie",
@@ -11,15 +11,15 @@ export const strings = Object.freeze({
     MY_EVENTS: "/wydarzenia",
     MY_PROFILE: "/mojprofil",
     LINKEDIN: "https://www.linkedin.com/in/pawe%C5%82-del-priore-b55bbb1a8",
-    SERVER_REQUEST: "http://localhost:4000/graphql",
+    SERVER_REQUEST: "https://localhost:4000/graphql",
     USER_PHOTO: "#",
-    IMAGE_REQUEST: "http://localhost:4000",
-    SERVER_PATH: "http://localhost:4000",
-    MAP_REQUEST: "http://localhost:4000/heremaprender",
-    GET_ADDRESSES: "http://localhost:4000/heremaprequest",
-    GET_LOCATION: "http://localhost:4000/heremaplocation",
-    GET_EMOJIS: "http://localhost:4000/emojis",
-    GET_EMOJI_CAT: "http://localhost:4000/emojicategories",
+    IMAGE_REQUEST: "https://localhost:4000",
+    SERVER_PATH: "https://localhost:4000",
+    MAP_REQUEST: "https://localhost:4000/heremaprender",
+    GET_ADDRESSES: "https://localhost:4000/heremaprequest",
+    GET_LOCATION: "https://localhost:4000/heremaplocation",
+    GET_EMOJIS: "https://localhost:4000/emojis",
+    GET_EMOJI_CAT: "https://localhost:4000/emojicategories",
   },
   navbar: {
     navType: {
