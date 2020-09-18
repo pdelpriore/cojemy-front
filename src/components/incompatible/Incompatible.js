988f04aaeb6686833a4292ea2e307897abb035f9
pdelpriore <paul.del.priore29@gmail.com>
2020-09-18 15:59:14 +0200

    add incompatible component

--- src/components/incompatible/Incompatible.js
+++ src/components/incompatible/Incompatible.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
+import "./incompatible.css";
 
 const Incompatible = () => {
-  return <div>niekompatybilny suko !</div>;
+  return (
+    <div className="incompatible-main">
+      <Row className="mb-5" />
+      <Row>
+        <Col xs={1} />
+        <Col xs={10}>
+          <div className="incompatible-text">
+            Aplikacja niekompatybilna z urządzeniami mobilnymi
+          </div>
+        </Col>
+        <Col xs={1} />
+      </Row>
+    </div>
+  );
 };
 
 export default Incompatible;
