1d283c945cef67d4e984726938b6c3a1a3ab478d
pdelpriore <paul.del.priore29@gmail.com>
2020-09-20 09:45:20 +0200

    change gitignore

--- src/views/signup/Signup.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Navbar from "../../components/navbar/Navbar";
-
-const Signup = ({ match }) => {
-  let { path, url, isExact } = match;
-  return <></>;
-};
-
-export default Signup;
